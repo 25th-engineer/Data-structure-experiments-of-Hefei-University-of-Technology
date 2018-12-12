c9175c988793afe56395b2fd636d7039e6cc902b
Xef Utah <wenhuafly@qq.com>
2018-12-12 12:37:59 +0800

    Data structure experiment 4 (2) : chain queue
    Data structure experiment 4 (2) : chain queue
    数据结构实验4②：链式队列

--- StdAfx.h
+++ StdAfx.h
@@ -3,24 +3,42 @@
 //      are changed infrequently
 //
 
-#if !defined(AFX_STDAFX_H__8FA49CDF_FC99_4984_AB37_46921F7ED357__INCLUDED_)
-#define AFX_STDAFX_H__8FA49CDF_FC99_4984_AB37_46921F7ED357__INCLUDED_
+#if !defined(AFX_STDAFX_H__009A1125_2F9A_4B93_9830_51B9398EBC52__INCLUDED_)
+#define AFX_STDAFX_H__009A1125_2F9A_4B93_9830_51B9398EBC52__INCLUDED_
 
 #if _MSC_VER > 1000
 #pragma once
 #endif // _MSC_VER > 1000
 
+
 #include <stdc++.h>
+//#include <iostream>
 
 using namespace std;
 
 typedef int elementType;
 typedef char elementType1;
-const int maxn = 100;
+
+typedef struct node
+{
+	elementType data;
+	struct node *link;
+}LNode, *PNode;
+
+typedef struct charNode
+{
+	elementType1 data;
+	struct charNode *link;
+}CLNode, *CPNode;
+
+//typedef struct linkedQueue
+//{
+//	LNode *_front, *_rear;
+//}LQueue, *PQueue;
 
 // TODO: reference additional headers your program requires here
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
-#endif // !defined(AFX_STDAFX_H__8FA49CDF_FC99_4984_AB37_46921F7ED357__INCLUDED_)
+#endif // !defined(AFX_STDAFX_H__009A1125_2F9A_4B93_9830_51B9398EBC52__INCLUDED_)
